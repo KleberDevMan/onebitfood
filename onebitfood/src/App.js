01f2b63463466aeb914fddb83ef3bc99f8ea7332
KleberDevMen <klebersubcontas@gmail.com>
2019-05-13 18:31:31 -0300

    trabalhando no front-end

---
+++ onebitfood/src/App.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import "./App.scss";
+import { Container, Section } from "rbx";
+import Header from "./components/header";
+
+const App = () => (
+    <Fragment>
+        <Header/>
+        <Section>
+            <Container>
+                One Bit Food
+            </Container>
+        </Section>
+    </Fragment>
+);
+
+export default App;
+
