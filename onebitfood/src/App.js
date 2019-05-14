f37cd05bd47b83fc75abc39fd191040d398a12bc
KleberDevMen <klebersubcontas@gmail.com>
2019-05-14 14:23:58 -0300

    .

--- onebitfood/src/App.js
+++ onebitfood/src/App.js
@@ -2,13 +2,14 @@ import React, {Fragment} from 'react';
 import "./App.scss";
 import { Container, Section } from "rbx";
 import Header from "./components/header";
+import Routes from './routes'
 
 const App = () => (
     <Fragment>
         <Header/>
         <Section>
             <Container>
-                One Bit Food
+                <Routes/>
             </Container>
         </Section>
     </Fragment>
